27e9b221208ef6e5230f6fbb5c24b3b07d6ea0ce
Patrick McMorran <kingrolloinc@sbcglobal.net>
2013-12-15 23:21:17 -0500

    First Commit. Have copied most the api documentation inline with some unimplemented code, how hammering out functions and tests.

---
+++ Conveyor_JSONRPC_API_Tests/UnitTest1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Text;
+using Conveyor_JSONRPC_API;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+
+namespace Conveyor_JSONRPC_API_Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        IPEndPoint ipEndPoint;
+        TcpClient tcpClient;
+        Stream dataStream;
+
+        public UnitTest1()
+        {
+            ipEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9999);
+            tcpClient = new TcpClient();
+            tcpClient.Connect(ipEndPoint);
+            dataStream = tcpClient.GetStream();
+        }
+
+        [TestMethod]
+        public void A_Hello()
+        {
+            JsonRpcResult Response = CallMethod(dataStream, tcpClient, ServerAPI.Hello(2));
+            if (Response.result.Equals("world"))
+            {
+                return;
+            }
+            throw (new Exception());
+        }
+
+        [TestMethod]
+        public void GetPrinters()
+        {
+            JsonRpcResult Response = CallMethod(dataStream, tcpClient, ServerAPI.GetPrinters(3));
+            if (Response.result.Equals("world"))
+            {
+                return;
+            }
+            throw (new Exception());
+        }
+        
+        public static JsonRpcResult CallMethod(Stream dataStream, TcpClient tcpClient, string JsonMethod)
+        {
+            byte[] byteArray = Encoding.ASCII.GetBytes(JsonMethod);
+            dataStream.Write(byteArray, 0, byteArray.Length);
+            byte[] bytesToRead = new byte[tcpClient.ReceiveBufferSize];
+            int bytesRead = dataStream.Read(bytesToRead, 0, bytesToRead.Length);
+            string Reply = Encoding.ASCII.GetString(bytesToRead, 0, bytesRead);
+            return JsonConvert.DeserializeObject<JsonRpcResult>(Reply);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+Conveyor_JSONRPC_API
+==========
+
+This library is used to provide a means of control over Makerbot Replicator 2 using Conveyor's JSON API.
+
+Designed to communicate using the JSON API provided by Makerbot. (https://github.com/makerbot/conveyor/blob/master/doc/conveyor-jsonrpc-api.md)
